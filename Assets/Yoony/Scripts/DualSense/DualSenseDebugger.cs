30c5a1af55b334da7f8513a97dfe0b56642046e9
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2025-07-30 23:56:20 +0900

    feat: DualSenseInputManager

---
+++ Assets/Yoony/Scripts/DualSense/DualSenseDebugger.cs
@@ -0,0 +1,458 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace DualSenseUnity
+
+{
+   public class DualSenseDebugger : MonoBehaviour
+   {
+      [ReadOnly] public uint ControllerCount = 0;
+      public bool WriteDebugInfoToScreen = false;
+      public uint ControllerIndexToDebug = 0;
+      public List<ControllerInputState> ControllerInputs = new List<ControllerInputState>();
+      public ControllerOutputState ControllerOutput;
+
+      private List<DualSenseController> _controllers = new List<DualSenseController>();
+
+      void Start()
+      {
+         ControllerCount = DualSense.GetControllerCount();
+         DualSense.ControllerCountChanged += ControllerCountChanged;
+         _controllers = DualSense.GetControllers();
+      }
+
+      private void ControllerCountChanged()
+      {
+         ControllerCount = DualSense.GetControllerCount();
+         _controllers = DualSense.GetControllers();
+      }
+
+      void LateUpdate()
+      {
+         ControllerInputs.Clear();
+         foreach ( var controller in _controllers )
+         {
+            var inputState = controller.GetInputState();
+            ControllerInputs.Add( inputState );
+         }
+
+         var indexToDebug = GetCurrentValidControllerIndex();
+         if ( indexToDebug < _controllers.Count )
+         {
+            _controllers[(int)indexToDebug].SetOutputState( ControllerOutput );
+         }
+      }
+
+      void OnGUI()
+      {
+         if ( WriteDebugInfoToScreen )
+         {
+            var indexToDebug = GetCurrentValidControllerIndex();
+            if ( indexToDebug < ControllerInputs.Count )
+            {
+               GUI.TextArea( new Rect( 10, 10, 350, 805 ), GetControllerDebugString( indexToDebug, ControllerInputs[(int)indexToDebug] ) );
+            }
+         }
+      }
+
+      private uint GetCurrentValidControllerIndex()
+      {
+         var indexToDebug = Math.Max( 0, ControllerIndexToDebug );
+         indexToDebug = Math.Min( indexToDebug, ControllerCount - 1 );
+
+         return indexToDebug;
+      }
+
+      private string GetControllerDebugString( uint controllerIndex, ControllerInputState inputState )
+      {
+         var sb = new StringBuilder();
+
+         sb.AppendLine( $"Controller Count: {ControllerCount}" );
+         sb.AppendLine();
+
+         sb.AppendLine( $"******************** DualSense Controller {controllerIndex} ********************" );
+         sb.AppendLine();
+         sb.AppendLine( "--------------------------------------------------------------------------------------" );
+         sb.AppendLine();
+
+         if ( inputState.IsValid )
+         {
+            sb.AppendLine( $"Left Trigger: {inputState.LeftTrigger.TriggerValue}\t\tRight Trigger: {inputState.RightTrigger.TriggerValue}" );
+            sb.AppendLine( $"As Button: {GetButtonString( inputState.LeftTrigger.ActiveState )}\tAs Button: {GetButtonString( inputState.RightTrigger.ActiveState )}" );
+            sb.AppendLine();
+
+            sb.AppendLine( $"Left Bumper: {GetButtonString( inputState.LeftBumper )}\tRight Bumper: {GetButtonString( inputState.RightBumper )}" );
+
+            sb.AppendLine();
+            sb.AppendLine( "--------------------------------------------------------------------------------------" );
+            sb.AppendLine();
+
+            sb.AppendLine( $"Create Button: {GetButtonString( inputState.CreateButton )}\tOptions Button: {GetButtonString( inputState.OptionsButton )}" );
+
+            sb.AppendLine();
+            sb.AppendLine( "--------------------------------------------------------------------------------------" );
+            sb.AppendLine();
+
+            sb.AppendLine( $"   Left Stick:\t\t   Right Stick:" );
+            sb.AppendLine();
+            sb.AppendLine( $"X: {GetNumberString( inputState.LeftStick.XAxis, 2, 2 )} Y: { GetNumberString( inputState.LeftStick.YAxis, 2, 2 )}\t\tX: {GetNumberString( inputState.RightStick.XAxis, 2, 2 )} Y: {GetNumberString( inputState.RightStick.YAxis, 2, 2 )}" );
+            sb.AppendLine( $"As Button: {GetButtonString( inputState.LeftStick.PushButton )}\tAs Button: {GetButtonString( inputState.RightStick.PushButton )}" );
+
+            sb.AppendLine();
+            sb.AppendLine( "--------------------------------------------------------------------------------------" );
+            sb.AppendLine();
+
+            sb.Append( GetDpadAndFaceString( inputState ) );
+
+            sb.AppendLine();
+            sb.AppendLine( "--------------------------------------------------------------------------------------" );
+            sb.AppendLine();
+
+            sb.AppendLine( $"PS Button: {GetButtonString( inputState.PSButton )}\tMic Button: {GetButtonString( inputState.MicrophoneButton )}" );
+
+            sb.AppendLine();
+            sb.AppendLine( "--------------------------------------------------------------------------------------" );
+            sb.AppendLine();
+
+            sb.AppendLine( $"Touch Finger 1:\t\tTouch Finger 2:" );
+            sb.AppendLine( $"X: {GetNumberString( inputState.TouchPad.TouchPoint1.X, 2, 2 )} Y: {GetNumberString( inputState.TouchPad.TouchPoint1.Y, 2, 2 )}\t\tX: {GetNumberString( inputState.TouchPad.TouchPoint2.X, 2, 2 )} Y: {GetNumberString( inputState.TouchPad.TouchPoint2.Y, 2, 2 )}" );
+            sb.AppendLine();
+
+            sb.AppendLine();
+            sb.AppendLine( "--------------------------------------------------------------------------------------" );
+            sb.AppendLine();
+
+            sb.AppendLine( $"Accelerometer:\t\tGyroscope:" );
+            sb.AppendLine( $"X: {GetNumberString( inputState.Accelerometer.XAxis, 0, 5 )}\t\t\tX: {GetNumberString( inputState.Gyroscope.XAxis, 0, 6 )}" );
+            sb.AppendLine( $"Y: {GetNumberString( inputState.Accelerometer.YAxis, 0, 5 )}\t\t\tY: {GetNumberString( inputState.Gyroscope.YAxis, 0, 6 )}" );
+            sb.AppendLine( $"Z: {GetNumberString( inputState.Accelerometer.ZAxis, 0, 5 )}\t\t\tZ: {GetNumberString( inputState.Gyroscope.ZAxis, 0, 6 )}" );
+            sb.AppendLine();
+
+            sb.AppendLine();
+            sb.AppendLine( "--------------------------------------------------------------------------------------" );
+            sb.AppendLine();
+
+            //sb.AppendLine( $"Percent Charged: {inputState.Battery.PercentCharged}\tFully Charged: {inputState.Battery.FullyCharged}\tCharging: {inputState.Battery.Charging}" );
+            //sb.AppendLine();
+            sb.AppendLine( $"\tHeadphones Connected: {inputState.HeadPhonesConnected}" );
+         }
+         else
+         {
+            sb.AppendLine( "Count not retrieve controller input state" );
+         }
+
+         return sb.ToString();
+      }
+      private string GetButtonString( ButtonState state )
+      {
+         switch ( state )
+         {
+            case ButtonState.Down:
+               {
+                  return "DOWN      ";
+               }
+            case ButtonState.Up:
+               {
+                  return "UP           ";
+               }
+            case ButtonState.NewDown:
+               {
+                  return "NEW DOWN";
+               }
+            case ButtonState.NewUp:
+               {
+                  return "NEW UP   ";
+               }
+            default:
+               {
+                  return string.Empty;
+               }
+         }
+      }
+
+      private string GetDpadAndFaceString( ControllerInputState state )
+      {
+         var sb = new StringBuilder();
+         sb.AppendLine( "      D-pad:\t\t\t   Face Buttons:" );
+         sb.AppendLine();
+
+         var uArrow = state.DPadUpButton == ButtonState.Down ? "↑" : " ";
+         var dArrow = state.DPadDownButton == ButtonState.Down ? "↓" : " ";
+         var lArrow = state.DPadLeftButton == ButtonState.Down ? "←" : "   ";
+         var rArrow = state.DPadRightButton == ButtonState.Down ? "→" : "   ";
+
+         var uFace = state.TriangleButton == ButtonState.Down ? "△" : " ";
+         var dFace = state.CrossButton == ButtonState.Down ? "X" : " ";
+         var lFace = state.SquareButton == ButtonState.Down ? "□" : "  ";
+         var rFace = state.CircleButton == ButtonState.Down ? "◯" : " ";
+
+         sb.AppendLine( $"          {uArrow}\t\t\t            {uFace}" );
+         sb.AppendLine( $"     {lArrow}  +  {rArrow}                                       {lFace}  +  {rFace}" );
+         sb.AppendLine( $"          {dArrow}\t\t\t            {dFace}" );
+
+         return sb.ToString();
+      }
+
+      private static string GetNumberString( double num, int decimalPlaces, int fixedLength )
+      {
+         var decimalString = "{0:0";
+         if ( decimalPlaces > 0 )
+         {
+            decimalString += ".";
+         }
+         for ( var i = 0; i < decimalPlaces; i++ )
+         {
+            decimalString += "0";
+         }
+         decimalString += "}";
+
+         decimalString = string.Format( decimalString, num );
+
+         if ( num >= 0 )
+         {
+            decimalString = " " + decimalString;
+         }
+
+         while ( (num > 0 && decimalString.Length < fixedLength - 1) || (decimalString.Length < fixedLength) )
+         {
+            decimalString += " ";
+         }
+
+         return decimalString;
+      }
+   }
+
+    [CustomEditor( typeof( DualSenseDebugger ) )]
+
+    [CanEditMultipleObjects]
+
+
+    public class DualSenseDebuggerEditor : Editor
+   {
+      private SerializedProperty _controllerCount;
+      private SerializedProperty _writeDebugInfoToScreen;
+      private SerializedProperty _controllerIndexToDebug;
+      private SerializedProperty _controllerInputs;
+      private SerializedProperty _controllerOutput;
+
+      void OnEnable()
+      {
+         _writeDebugInfoToScreen = serializedObject.FindProperty( "WriteDebugInfoToScreen" );
+         _controllerCount = serializedObject.FindProperty( "ControllerCount" );
+         _controllerIndexToDebug = serializedObject.FindProperty( "ControllerIndexToDebug" );
+         _controllerInputs = serializedObject.FindProperty( "ControllerInputs" );
+         _controllerOutput = serializedObject.FindProperty( "ControllerOutput" );
+      }
+
+      public override void OnInspectorGUI()
+      {
+         serializedObject.Update();
+
+         EditorGUILayout.LabelField( "General" );
+         using ( new EditorGUI.IndentLevelScope() )
+         {
+            if ( _controllerCount != null )
+            {
+               EditorGUILayout.PropertyField( _controllerCount );
+            }
+            if ( _writeDebugInfoToScreen != null )
+            {
+               EditorGUILayout.PropertyField( _writeDebugInfoToScreen );
+            }
+            if ( _controllerIndexToDebug != null )
+            {
+               EditorGUILayout.PropertyField( _controllerIndexToDebug );
+            }
+         }
+
+         if ( _controllerInputs != null )
+         {
+            EditorGUILayout.LabelField( "Controller Input Readings" );
+            using ( new EditorGUI.DisabledScope( true ) )
+            {
+               using ( new EditorGUI.IndentLevelScope() )
+               {
+                  using ( new EditorGUI.IndentLevelScope() )
+                  {
+                     EditorGUILayout.PropertyField( _controllerInputs );
+                  }
+               }
+            }
+         }
+
+         if ( _controllerOutput != null )
+         {
+            EditorGUILayout.LabelField( "Controller Output Settings" );
+            using ( new EditorGUI.IndentLevelScope() )
+            {
+               var leftTriggerEffect = _controllerOutput.FindPropertyRelative( "LeftTriggerEffect" );
+               if ( leftTriggerEffect != null )
+               {
+                  EditorGUILayout.LabelField( "Left Trigger" );
+                  using ( new EditorGUI.IndentLevelScope() )
+                  {
+                     ShowTriggerEffectProperty( leftTriggerEffect );
+                  }
+               }
+
+               var rightTriggerEffect = _controllerOutput.FindPropertyRelative( "RightTriggerEffect" );
+               if ( rightTriggerEffect != null )
+               {
+                  EditorGUILayout.LabelField( "Right Trigger" );
+                  using ( new EditorGUI.IndentLevelScope() )
+                  {
+                     ShowTriggerEffectProperty( rightTriggerEffect );
+                  }
+               }
+
+               var lightBarEnabled = _controllerOutput.FindPropertyRelative( "LightBarEnabled" );
+               if ( lightBarEnabled != null )
+               {
+                  EditorGUILayout.PropertyField( lightBarEnabled, new GUIContent( "Light Bar Enabled" ) );
+                  if ( lightBarEnabled.boolValue )
+                  {
+                     using ( new EditorGUI.IndentLevelScope() )
+                     {
+                        var lightBarIntensity = _controllerOutput.FindPropertyRelative( "LightBarIntensity" );
+                        if ( lightBarIntensity != null )
+                        {
+                           EditorGUILayout.PropertyField( lightBarIntensity );
+                        }
+                        var lightBarR = _controllerOutput.FindPropertyRelative( "LightBarR" );
+                        if ( lightBarR != null )
+                        {
+                           EditorGUILayout.PropertyField( lightBarR );
+                        }
+                        var lightBarG = _controllerOutput.FindPropertyRelative( "LightBarG" );
+                        if ( lightBarG != null )
+                        {
+                           EditorGUILayout.PropertyField( lightBarG );
+                        }
+                        var lightBarB = _controllerOutput.FindPropertyRelative( "LightBarB" );
+                        if ( lightBarB != null )
+                        {
+                           EditorGUILayout.PropertyField( lightBarB );
+                        }
+                     }
+                  }
+               }
+
+               EditorGUILayout.LabelField( "Player Lights" );
+               using ( new EditorGUI.IndentLevelScope() )
+               {
+                  var leftPlayerLightEnabled = _controllerOutput.FindPropertyRelative( "LeftPlayerLightEnabled" );
+                  if ( leftPlayerLightEnabled != null )
+                  {
+                     EditorGUILayout.PropertyField( leftPlayerLightEnabled );
+                  }
+                  var middleLeftPlayerLightEnabled = _controllerOutput.FindPropertyRelative( "MiddleLeftPlayerLightEnabled" );
+                  if ( middleLeftPlayerLightEnabled != null )
+                  {
+                     EditorGUILayout.PropertyField( middleLeftPlayerLightEnabled );
+                  }
+                  var middlePlayerLightEnabled = _controllerOutput.FindPropertyRelative( "MiddlePlayerLightEnabled" );
+                  if ( middlePlayerLightEnabled != null )
+                  {
+                     EditorGUILayout.PropertyField( middlePlayerLightEnabled );
+                  }
+                  var middleRightPlayerLightEnabled = _controllerOutput.FindPropertyRelative( "MiddleRightPlayerLightEnabled" );
+                  if ( middleRightPlayerLightEnabled != null )
+                  {
+                     EditorGUILayout.PropertyField( middleRightPlayerLightEnabled );
+                  }
+                  var rightPlayerLightEnabled = _controllerOutput.FindPropertyRelative( "RightPlayerLightEnabled" );
+                  if ( rightPlayerLightEnabled != null )
+                  {
+                     EditorGUILayout.PropertyField( rightPlayerLightEnabled );
+                  }
+                  var fadePlayerLight = _controllerOutput.FindPropertyRelative( "FadePlayerLight" );
+                  if ( fadePlayerLight != null )
+                  {
+                     EditorGUILayout.PropertyField( fadePlayerLight );
+                  }
+               }
+            }
+         }
+
+         serializedObject.ApplyModifiedProperties();
+      }
+
+        private void ShowTriggerEffectProperty( SerializedProperty triggerEffect )
+      {
+         var effectType = triggerEffect.FindPropertyRelative( "EffectType" );
+         if ( effectType != null && effectType.enumValueIndex >= 0 )
+         {
+            var showStartPos = false;
+            var showEndPos = false;
+            var showBeginForce = false;
+            var showMiddleForce = false;
+            var showEndForce = false;
+            var showFreq = false;
+            var showKeepEffect = false;
+
+            var effectName = effectType.enumNames[effectType.enumValueIndex];
+            switch ( effectName )
+            {
+               case "NoResistance":
+                  {
+                     break;
+                  }
+               case "ContinuousResistance":
+                  {
+                     showStartPos = showBeginForce = true;
+                     break;
+                  }
+               case "SectionResistance":
+                  {
+                     showStartPos = showEndPos = showBeginForce = true;
+                     break;
+                  }
+               case "EffectEx":
+                  {
+                     showStartPos = showBeginForce = showMiddleForce = showEndForce = showFreq = showKeepEffect = true;
+                     break;
+                  }
+               default:
+                  {
+                     break;
+                  }
+            }
+
+            EditorGUILayout.PropertyField( effectType );
+            if ( showStartPos )
+            {
+               EditorGUILayout.PropertyField( triggerEffect.FindPropertyRelative( "StartPosition" ) );
+            }
+            if ( showEndPos )
+            {
+               EditorGUILayout.PropertyField( triggerEffect.FindPropertyRelative( "EndPosition" ) );
+            }
+            if ( showBeginForce )
+            {
+               EditorGUILayout.PropertyField( triggerEffect.FindPropertyRelative( "BeginForce" ) );
+            }
+            if ( showMiddleForce )
+            {
+               EditorGUILayout.PropertyField( triggerEffect.FindPropertyRelative( "MiddleForce" ) );
+            }
+            if ( showEndForce )
+            {
+               EditorGUILayout.PropertyField( triggerEffect.FindPropertyRelative( "EndForce" ) );
+            }
+            if ( showFreq )
+            {
+               EditorGUILayout.PropertyField( triggerEffect.FindPropertyRelative( "Frequency" ) );
+            }
+            if ( showKeepEffect )
+            {
+               EditorGUILayout.PropertyField( triggerEffect.FindPropertyRelative( "KeepEffect" ) );
+            }
+         }
+      }
+   }
+}
+#endif
---
+++ Assets/Yoony/Scripts/InputManager/DualSenseInputManager.cs
@@ -0,0 +1,152 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DualSenseUnity;
+using UnityEngine.UIElements;
+
+public class DualSenseInputManager : MonoBehaviour
+{
+    public static DualSenseInputManager Instance { get; private set; }
+    
+    [Header("Rumble Pattern")]
+    public AnimationCurve SwimRumbleCurve;
+    public float SwimRumbleDuration = 2f;
+    
+    [HideInInspector]
+    public uint ControllerCount = 0;
+    public List<DualSenseController> DualSenseControllers = new List<DualSenseController>();
+    
+    private ControllerOutputState _outputState = new ControllerOutputState();
+    
+    private Coroutine dualSenseRumbleCoroutine;
+    
+    private void Awake()
+    {
+        if (!Instance)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+    
+    private void RefreshControllers()
+    {
+        ControllerCount = DualSense.GetControllerCount();
+        DualSenseControllers = DualSense.GetControllers();
+    }
+
+    private void Start()
+    {
+        DualSense.ControllerCountChanged += RefreshControllers;
+        RefreshControllers();
+    }
+    
+    /**
+     * [DualSense inputs]
+     * 
+     * DPad Buttons(bool): inputState.DPad[Up|Down|Left|Right]Button.[Up|NewUp|Down|NewDown]
+     * Action Buttons(bool): inputState.[Circle|Cross|Square|Triangle]Button.[Up|NewUp|Down|NewDown]
+     * Stick Buttons(bool): inputState.[Left|Right]Stick.PushButton
+     * Stick Tilt(double)[-1.0, 1.0]: inputState.[Left|Right]Stick.[X|Y]Axis
+     * R1|L1 Buttons(bool): inputState.[Left|Right]Bumper.[Up|NewUp|Down|NewDown]
+     * R2|L2 Buttons(bool): inputState.[Lef|Right]Trigger.ActiveState.[Up|NewUp|Down|NewDown]
+     * R2|L2 Buttons Trigger(double)[0.0, 1.0]: inputState.[Lef|Right]Trigger.TriggerValue
+     * Create Button(bool): inputState.CreateButton.[Up|NewUp|Down|NewDown]
+     * Options Button(bool): inputState.OptionsButton.[Up|NewUp|Down|NewDown]
+     * PS Button(bool): inputState.PSButton.[Up|NewUp|Down|NewDown]
+     * Microphone Button(bool): inputState.MicrophoneButton.[Up|NewUp|Down|NewDown]
+     * TouchPad Points(double)[0.0, 1.0]: inputState.TouchPad.TouchPoint[1|2].[X|Y] -> X [0.0, 1.0] means [Left, Right], Y [0.0, 1.0] means [Up, Down]
+     * Accelerometer(double)[0.0, 1.0]: inputState.Accelerometer.[X|Y|Z]Axis
+     * Gyroscope(double)[0.0, 1.0]: inputState.Gyroscope.[X|Y|Z]Axis
+     * HeadPhone Connection(bool): inputState.HeadPhonesConnected
+     *
+     * [DualSense Outputs]
+     *
+     * Rumble: _outputState.[Right|Left]RumbleIntensity = [0.0, 1.0]
+     * 
+     */
+    
+    private bool IsButtonPressed( ButtonState buttonState )
+    {
+        return buttonState == ButtonState.NewDown;
+    }
+
+    private bool IsButtonBeingPressed(ButtonState buttonState)
+    {
+        return buttonState == ButtonState.Down || buttonState == ButtonState.NewDown;
+    }
+
+    private bool IsButtonReleased(ButtonState buttonState)
+    {
+        return buttonState == ButtonState.NewUp;
+    }
+
+    public void RumbleController(float intensity)
+    {
+        _outputState.LeftRumbleIntensity = intensity;
+        _outputState.RightRumbleIntensity = intensity;
+    }
+
+    public void RumbleControllerWithCurve(AnimationCurve curve, float duration)
+    {
+        if (dualSenseRumbleCoroutine != null)
+            StopCoroutine(dualSenseRumbleCoroutine);
+
+        dualSenseRumbleCoroutine = StartCoroutine(RumblePatternWithCurve(curve, duration));
+    }
+    
+    IEnumerator RumblePatternWithCurve(AnimationCurve curve, float duration)
+    {
+        float timer = 0f;
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+            
+            _outputState.LeftRumbleIntensity = curve.Evaluate(timer / duration);
+            _outputState.RightRumbleIntensity = curve.Evaluate(timer / duration);
+            
+            yield return null;
+        }
+
+        _outputState.LeftRumbleIntensity = 0f;
+        _outputState.RightRumbleIntensity = 0f;
+    }
+
+    public void RumbleControllerForDuration(float intensity, float duration)
+    {
+        if (dualSenseRumbleCoroutine != null)
+            StopCoroutine(dualSenseRumbleCoroutine);
+        
+        dualSenseRumbleCoroutine = StartCoroutine(RumblePattern(intensity, duration));
+    }
+    
+    IEnumerator RumblePattern(float intensity, float duration)
+    {
+        _outputState.LeftRumbleIntensity = intensity;
+        _outputState.RightRumbleIntensity = intensity;  
+        yield return new WaitForSeconds(duration);
+        _outputState.LeftRumbleIntensity = 0f;
+        _outputState.RightRumbleIntensity = 0f;
+    }
+
+    private void Update()
+    {
+        var controllerToUse = DualSenseControllers[0];
+        var inputState = controllerToUse.GetInputState();
+        
+        // interact with DualSense logic
+        if (IsButtonPressed( inputState.DPadUpButton ))
+            Debug.Log("DPad Up pressed");
+        
+        if (IsButtonPressed( inputState.CreateButton ))
+            Debug.Log("Create Btn pressed");
+        
+        if (IsButtonPressed( inputState.LeftTrigger.ActiveState ))
+            RumbleControllerForDuration(0.2f, 1f);
+        
+        if (IsButtonPressed( inputState.RightTrigger.ActiveState ))
+            RumbleControllerWithCurve(SwimRumbleCurve, SwimRumbleDuration);
+        
+        controllerToUse.SetOutputState( _outputState );
+    }
+}
