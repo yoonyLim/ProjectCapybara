83c394c8c2773ed9e6df40d8657a8657b966b953
Seunghwan <slee1009@khu.ac.kr>
2025-10-03 09:38:13 +0900

    feat: distance fog, depth of field, jump pad animation, distant moutains

--- Assets/Seunghwan/Scripts/Platforms/LogPlatform.cs
+++ Assets/Seunghwan/Scripts/Platforms/LogPlatform.cs
@@ -6,8 +6,8 @@ public class LogPlatform : MonoBehaviour
     [SerializeField] private float startTimeOffset = 3f;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float moveDistanceRatio = 1f;
-    private Collider collider;
-    private Rigidbody rigidbody;
+    private Collider platformCollider;
+    private Rigidbody platformRigidbody;
     
     
     private Vector3 startPosition;
@@ -15,16 +15,16 @@ public class LogPlatform : MonoBehaviour
 
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
+        platformRigidbody = GetComponent<Rigidbody>();
+        platformCollider = GetComponent<Collider>();
         startPosition = transform.position;
-        moveDistance = collider.bounds.size.y * moveDistanceRatio;
+        moveDistance = platformCollider.bounds.size.y * moveDistanceRatio;
     }
 
     private void FixedUpdate()
     {
         float currentPositionOffset = Mathf.PingPong(Time.time * moveSpeed + startTimeOffset, moveDistance);
-        rigidbody.MovePosition(startPosition - transform.up * currentPositionOffset);
+        platformRigidbody.MovePosition(startPosition - transform.up * currentPositionOffset);
     }
     
 }
