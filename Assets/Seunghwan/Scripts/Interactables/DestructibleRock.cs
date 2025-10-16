3993eeb6a67ac9ffcbfcb5002acb774fe3a3f8fc
tndmstla <jineung78@gmail.com>
2025-10-17 03:07:07 +0900

    [FEAT] BreakableRock 기능 구현

--- Assets/Seunghwan/Scripts/Interactables/DestructibleRock.cs
+++ Assets/Seunghwan/Scripts/Interactables/DestructibleRock.cs
@@ -1,16 +1,18 @@
 using System;
+using Unity.VisualScripting;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class DestructibleRock : MonoBehaviour, IDestructible
 {
-    private ParticleSystem rockParticleSystem;
+    //private ParticleSystem rockParticleSystem;
     private Collider rockCollider;
     private MeshRenderer rockMeshRenderer;
-    
-
+    [SerializeField] private Transform brokenRock;
+    [SerializeField] Vector3 brokenPosition;
     private void Awake()
     {
-        rockParticleSystem = GetComponent<ParticleSystem>();
+        //rockParticleSystem = GetComponent<ParticleSystem>();
         rockCollider = GetComponent<Collider>();
         rockMeshRenderer = GetComponent<MeshRenderer>();
     }
@@ -19,8 +21,17 @@ public class DestructibleRock : MonoBehaviour, IDestructible
     {
         rockCollider.enabled = false;
         rockMeshRenderer.enabled = false;
-        rockParticleSystem.Play();
-        transform.GetChild(0).gameObject.SetActive(false);
+        brokenRock.gameObject.SetActive(true);
+        foreach (Transform child in brokenRock)
+        {
+            if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(60f, brokenPosition, 5f);
+                Destroy(brokenRock.gameObject, 5f);
+            }
+            Destroy(gameObject, 5f);
+        }
+        //transform.GetChild(0).gameObject.SetActive(false);
     }
 
     
