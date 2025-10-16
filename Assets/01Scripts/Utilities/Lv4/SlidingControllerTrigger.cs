ecc8a87be063e3e9271beeb68156c923b93088d6
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2025-10-17 00:42:41 +0900

    feat: lv4 player controller

---
+++ Assets/01Scripts/Utilities/Lv4/SlidingControllerTrigger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SlidingControllerTrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("controller trigger OnCollisionEnter");
+        }
+    }
+}
